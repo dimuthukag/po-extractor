e49ec75202004dd9f6b047fa36500a061695c014
Dimuthu Gunawardhana <dimuthukag@gmail.com>
2023-07-13 09:20:07 +0530

    feat: add image based po files extraction support
    Implement the software support for image based PO files data extraction

--- po_formats/po_base.py
+++ po_formats/po_base.py
@@ -1,5 +1,8 @@
 import os
 import pypdfium2
+import pdf2image
+import cv2
+import pytesseract
 
 class PO_BASE:
     """
@@ -9,7 +12,38 @@ class PO_BASE:
         if not os.path.exists(poDocFilepath):
             raise FileNotFoundError
         self.__poDocFilepath = poDocFilepath
-        self.__poDoc = pypdfium2.PdfDocument(poDocFilepath)
+        self.__poDoc = self.__getPoDocContent()
+
+    def __getPoDocContent(self)->list:
+        self.__poDoc = pypdfium2.PdfDocument(self.__poDocFilepath)
+
+        if self.getPage(1)!="":
+            return self.__poDoc
+        else:
+            poDocContent= []
+            os.makedirs("../../temp",exist_ok=True)
+            try:
+                pages = pdf2image.pdf2image.convert_from_path(self.__poDocFilepath,500,poppler_path=r"./poppler-0.68.0/bin")
+            except pdf2image.exceptions.PDFInfoNotInstalledError:
+                pages = pdf2image.pdf2image.convert_from_path(self.__poDocFilepath,500,poppler_path=r"../poppler-0.68.0/bin")
+
+            try:
+                pytesseract.pytesseract.tesseract_cmd = r"./Tesseract-OCR/tesseract.exe"
+            except pytesseract.pytesseract.TesseractNotFoundError:
+                pytesseract.pytesseract.tesseract_cmd = r"../Tesseract-OCR/tesseract.exe"
+
+            for pageIndex in range(len(pages)):
+                tempImageFilepath = f'../../temp/{os.path.basename(self.__poDocFilepath)} - {pageIndex+1}.jpeg'
+                pages[pageIndex].save(tempImageFilepath, 'JPEG')
+
+                tempImage = cv2.imread(tempImageFilepath)
+                tempImage = cv2.resize(tempImage,None, fx=2.2, fy=2.2)
+                tempImage = cv2.cvtColor(tempImage, cv2.COLOR_BGR2GRAY)
+                pageText = pytesseract.image_to_string(tempImage)
+                poDocContent.append(pageText)
+                os.remove(tempImageFilepath)
+            os.removedirs("../../temp")
+            return poDocContent
 
     def numPages(self) -> int:
         """
@@ -22,5 +56,8 @@ class PO_BASE:
             Returns a specific page of the purchase order document
         """
         if type(pageNumber)==int and pageNumber>=0 and pageNumber<=self.numPages():
-            return self.__poDoc[pageNumber-1].get_textpage().get_text_range()
+            try:
+                return self.__poDoc[pageNumber-1].get_textpage().get_text_range()
+            except AttributeError:
+                return self.__poDoc[pageNumber-1]
         return None
