28306744101ac1ff2e87f32fc2f9fcb9a8b3b581
Dimuthu Gunawardhana <dimuthukag@gmail.com>
2023-07-13 18:24:39 +0530

    build: Add feature for auto delete previous builds

--- build.ps1
+++ build.ps1
@@ -1,3 +1,12 @@
+if (Test-Path ".\build") {
+    Remove-Item .\build -Recurse
+}
+if (Test-Path ".\dist") {
+    Remove-Item .\dist -Recurse
+}
+if (Test-Path ".\po-extractor.spec") {
+    Remove-Item .\po-extractor.spec
+}
 .\venv\Scripts\activate
 pyinstaller .\po-extractor.py --onefile --noconsole --add-binary ".\venv\Lib\site-packages\pypdfium2\pdfium.dll;." 
 Copy-Item .\po_std_format.xlsx .\dist\po_std_format.xlsx
